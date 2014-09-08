4528475883d057846ebeb92ff6129a84013873dc
huisama <huisama@outlook.com>
2014-09-08 22:12:05 +0800

    9/8

--- default.aspx
+++ default.aspx
@@ -5,15 +5,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>百度地图demo</title>
+    <title>百度地图 demo</title>
     <link href="default.css" rel="stylesheet" />
+    <script src="Scripts/jquery-1.8.2.min.js"></script>
+    <script src="http://api.map.baidu.com/api?v=1.5&ak=6a8f0766a2f6a433b6e171a90e6b8fe0"></script>
     <script src="default.js"></script>
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>6a8f0766a2f6a433b6e171a90e6b8fe0
-    
+    <div id="main-content">
+        <div id="map"></div>
+        <aside id="aside-bar">
+            <button id="btn-clear-points" type="button">清空</button>
+            <button id="btn-create-polygon" type="button">创建</button>
+            <ol id="points"></ol>
+        </aside>
     </div>
-    </form>
 </body>
 </html>
--- default.js
+++ default.js
@@ -1,1 +1,60 @@
-﻿
+﻿/// <reference path="C:\Users\huisama\Documents\GitHub\BMapPolygonDemo\Scripts/bmap.getscript.cache.js" />
+
+function program() {
+
+    var map = null;
+    var polygon = new BMap.Polygon([], {
+        strokeWeight: 2, strokeColor: "#ff0000", fillColor: "#555", fillOpacity: 0.5 });
+    var $points = $("#points");
+
+    // "清除".
+    $("#btn-clear-points").on("click", function () {
+        $points.children().remove();
+        polygon.setPath([]);
+        map.removeOverlay(polygon);
+    });
+
+    $("#btn-create-polygon").on("click", function () {
+        // 封闭多边形
+        var path = polygon.getPath();
+        if (path.length > 0) {
+            path.push(path[path.length - 1]);
+            polygon.setPath(path);
+        }
+
+        map.addOverlay(polygon);
+        //map.setViewport(polygon.getPath());
+    });
+
+    // 构造函数.
+    function constructor() {
+        initMap();
+    }
+
+    // 初始地图
+    function initMap() {
+        map = new BMap.Map("map");          // 创建地图实例  
+        mapCenter = new BMap.Point(116.404, 39.915);  // 创建点坐标  
+        map.centerAndZoom(mapCenter, 19);
+        map.addEventListener("click", function (e) {
+            addItemToPoints(e.point);
+            var path = polygon.getPath();
+            path.push(e.point);    
+        });
+    };
+
+    // 
+    function addItemToPoints(point) {
+        var $item = $(document.createElement("li"));
+        $item.data("point", point);
+        $item.text("[lng:" + point.lng + ", lat:" + point.lat);
+        $item.appendTo($points);
+    }
+
+    //// call constructor
+    constructor();
+}
+
+$(function () {
+    var app = new program();
+});
